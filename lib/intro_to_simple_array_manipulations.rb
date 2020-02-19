40c43e85250938bf12f10108545e61fa16025228
Don Mallory <dmm333@gmail.com>
2020-02-19 21:11:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element,4)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
@@ -45,3 +45,4 @@ end
 def using_delete_at(array, integer)
   array.delete_at(integer)
 end
+
